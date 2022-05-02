f043aa83e36040ae6096c28b4f607cb5414b2b39
ek3waka <mansurov.eduard96@gmail.com>
2022-05-03 00:20:03 +0300

    started

---
+++ src/index.js
@@ -0,0 +1,40 @@
+const wrapper = document.querySelector('.wrapper')
+const header = document.createElement(header)
+wrapper.appendChild(header)
+
+const navbar = document.createElement(div)
+navbar.classList.add('navbar')
+header.appendChild(navbar)
+
+const home = document.createElement(div)
+const menu = document.createElement(div)
+const contact = document.createElement(div)
+
+home.classList.add('navbar-item', 'active')
+menu.classList.add('navbar-item')
+contact.classList.add('navbar-item')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const card1 = document.createElement(div)
+
+
+mainNode.appendChild(card1);
+mainNode.appendChild(card1);
+mainNode.appendChild(card1);
+mainNode.appendChild(card1);
